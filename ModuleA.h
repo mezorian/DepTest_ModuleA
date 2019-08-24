97921d7d9716efa38c7ebbcb222879aebcf0c0f1
Mezorian <mezorian@inc-ffh.solutions>
2019-08-24 22:23:00 +0200

    Update ModuleA.h

--- ModuleA.h
+++ ModuleA.h
@@ -3,16 +3,19 @@
 
 #include "mezorian/DepTest_Logging/Logger.h"
 
-using Logger = logger6::Logger;
+namespace submodule_a {
 
-class ModuleA {
-    public:
-        ModuleA() {}
-        void doSmth() {
-            Logger l;
-            l.log("ModuleA is doing smth");
-        }
-};
+    using Logger = logger8::Logger;
 
+    class ModuleA {
+        public:
+            ModuleA() {}
+            void doSmth() {
+                Logger l;
+                l.log("ModuleA is doing smth");
+            }
+    };
+
+}
 
 #endif // ModuleA_H
